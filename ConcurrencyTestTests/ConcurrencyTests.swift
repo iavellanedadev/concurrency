f857d85f8fd73027c54e8e23039dacb6a892d00a
Iban Avellaneda <iavellanedadev@gmail.com>
2020-04-01 02:28:47 -0400

    initial commit

--- ConcurrencyTestTests/ConcurrencyTests.swift
+++ ConcurrencyTestTests/ConcurrencyTests.swift
@@ -8,8 +8,31 @@ import XCTest
 @testable import ConcurrencyTest
 
 class ConcurrencyTests: XCTestCase {
+    
     func testloadMessage() {
         
+        let msgCaller = MessageCaller()
+        let dispatchQ = DispatchGroup()
+        
+        dispatchQ.enter()
+        
+        var message1 = String()
+        msgCaller.getMessage2{ (messageOne) in
+            message1 = messageOne
+            print("Message Receieved: \(message1)")
+            dispatchQ.leave()
+
+        }
+        
+        let result = dispatchQ.wait(timeout: DispatchTime.now() + 3)
+        
+        dispatchQ.notify(queue: .main)
+        {
+            if result == .success{
+                XCTAssertEqual(message1, "world")
+            }
+        }
+
     }
 
 }
--- README.md
+++ README.md
@@ -5,7 +5,4 @@
 * Open the Xcode project
 * Use Swift 4.2
 * Read the instructions in Concurrency.swift
-* After you have completed your test
-    * Create a zip file containing your code and the git history.
-        * Name this file ConcurrencyTest-YourName.zip
-    * Email this to your HSBC contact
+
